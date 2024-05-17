d9509f2864a1b32fed130bb7476e61aff0a1ed41
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-17 23:17:16 +0530

    committed

--- 02_variable.sh
+++ 02_variable.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 name="abhishek"
+age="24"
+college="amrutvahini"
+
+hostname= $HOSTNAME
 
 echo "my name is $name "
-
+echo "my age is $age"
+echo "my college name is $college"
+ech "hostname is $hostname"
 
 
