815bd3d7ed441f257a568b70a41b436eaef0ef88
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-18 19:32:23 +0530

    commited

--- 03_array.sh
+++ 03_array.sh
@@ -2,5 +2,8 @@
 
 myarray=(1 45 hello "How are you" 67)
 
-echo "The first value of array ${myarray[0]}"
+
+echo "The first value of array ${myarray[0]}"   # to print first value of array
+
+echo "values of arrays are ${myarray[*]}"       # to print all values of array
 
