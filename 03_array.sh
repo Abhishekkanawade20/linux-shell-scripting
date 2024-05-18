1f0821036db2d4563f4c7394a2d07c245a08e8c3
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-18 19:42:51 +0530

    commited

--- 03_array.sh
+++ 03_array.sh
@@ -6,8 +6,11 @@ myarray=(1 45 hello "How are you" 67)
 echo "The first value of array ${myarray[0]}"   # to print first value of array
 echo "values of arrays are ${myarray[*]}"       # to print all values of array
 
-#To find length value of array
+#To print length value of array
 echo "length of array ${#myarray[*]}"
+
+#How to get range values of arrays
+echo "values from index 2-3 ${myarray[*]:2:2}"
 
 
 
