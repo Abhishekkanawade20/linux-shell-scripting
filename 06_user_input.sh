72a6060542f5392dee9c6d28be7505488169abc7
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-19 00:27:31 +0530

    commited

--- 06_user_input.sh
+++ 06_user_input.sh
@@ -2,11 +2,12 @@
 
 #How to take input from user.
 
+echo Enter your name:
 read name
 echo "Your name is $name"
 
 #or
 
-read -p "what is your age" age
+read -p "what is your age: " age
 echo "my age is $age"
 
