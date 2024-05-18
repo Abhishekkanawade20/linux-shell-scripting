a5f536bdafc480d2e1107606c6e38047e004fccc
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-18 20:38:50 +0530

    commited

--- 05_string_ops.sh
+++ 05_string_ops.sh
@@ -5,8 +5,18 @@ myvarlength=${#myvar}                # we stored myvar length in another variabl
 
 echo "my var length is $myvarlength" # we are printing the length of var
 
-echo "upper case is ----${myvar^^}"
-echo "lower case is ----${myvar,,}"
+echo "upper case is ----${myvar^^}"   # It will print in upper case
+echo "lower case is ----${myvar,,}"   # It will print var in lower case
+
+#To replace a string
+newvar=${myvar/Hello/hi}
+
+echo "new var is ----$newvar"
+
+
+
+
+
 
 
 
