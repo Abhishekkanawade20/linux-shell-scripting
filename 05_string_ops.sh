b377adb65fe8b539f9bd76c65f369d4f18963798
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-18 20:33:17 +0530

    commited

--- 05_string_ops.sh
+++ 05_string_ops.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-myvar=("Hello how are you")
-myvarlength=${#myvar}
+myvar=("Hello how are you")          # we defined the variable
+myvarlength=${#myvar}                # we stored myvar length in another variable
 
-echo "my var length is $myvarlength"
+echo "my var length is $myvarlength" # we are printing the length of var
+
+echo "upper case is ----${myvar^^}"
+echo "lower case is ----${myvar,,}"
+
+
+
+
 
