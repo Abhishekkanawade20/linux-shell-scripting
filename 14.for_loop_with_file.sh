36b3c8d0567ed59be01081186fb57415712ea3ca
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-19 17:43:49 +0530

    commted

---
+++ 14.for_loop_with_file.sh
@@ -0,0 +1,10 @@
+# Here we are defining file location and reading that file's content using for loop script
+
+#!/bin/bash
+
+FILE= "/home/ec2-user/file1.txt"
+
+for name in $(cat $FILE)
+do
+    echo "user data is: $name"
+done
