ba25ee60a7c7f300152f2d02f9189d4d4a70a907
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-19 13:48:14 +0530

    commted

---
+++ 13_for_loop.sh
@@ -0,0 +1,13 @@
+# Here we will learn for loop
+
+for i in 1 2 3 4 5 6 7               # here is is variable, you can use anyone 
+do 
+    echo "number is : $i"
+done 
+
+  # for string
+
+for name in abhi vishal mayur
+do 
+    echo "name is : $name"
+done
