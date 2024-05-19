5aeadbaca7db07f14034b3b30f0efe5acc674386
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-19 19:11:21 +0530

    commit

--- 15_for_with_array.sh
+++ 15_for_with_array.sh
@@ -4,11 +4,9 @@
 
 myArray=(1 Hi 2 3 5 "Abhihske here" 6)
 
-length= $(#myArray[*])
+length=${#myArray[*]}
 
 for (( i=0;i<$length;i++ ))
 do 
-    echo "value of array is $(myArray[$i])"
+    echo "value of array is ${myArray[$i]}"
 done
-
-
