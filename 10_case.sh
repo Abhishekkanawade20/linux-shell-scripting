88b986beb7a2bbe8bcf20d54ac411e7fdb03192e
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-19 12:12:26 +0530

    commited

---
+++ 10_case.sh
@@ -0,0 +1,18 @@
+#how to take input from user and print that value accordingly.
+
+#!/bin/bash
+
+echo "Provide and option"
+echo " a option for print date"
+echo " b option for list"
+echo " c option to check current directory location"
+
+read choice              # It will take input from user
+
+case $choice in          # here we are defining case
+    a)date;;             # command to print date
+    b)ls;;               # command for list
+    c)pwd;;              # It will print pwd
+    *)echo "please provide valid input"
+esac
+
