7637dfcb9f39f52767fbfabc8b53895c0ee7d50f
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-19 19:06:17 +0530

    commited

---
+++ 15_for_with_array.sh
@@ -0,0 +1,13 @@
+#Here we creating for loop with array
+
+#!/bin/bash
+
+myArray=(1 Hi 2 3 5 "Abhihske here" 6)
+
+length= $(#myArray[*])
+
+for (( i=0;i<$length;i++ ))
+do 
+    echo "value of array is $myArray($i)"
+done
+
