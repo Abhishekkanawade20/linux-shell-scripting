a27b7047b0c6daa6eb71e40c27f801a0b538a196
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-19 22:50:35 +0530

    commit

---
+++ 21_while_with_csv.sh
@@ -0,0 +1,14 @@
+# Here using this script we can retreive required data from csv file.
+# This is used to when csv file contains lot of data and we required only specific data
+# You need to define file location to retrive data.
+
+#!/bin/bash
+
+while IFS="," read id name age department #(id name age dept are the variables)
+do
+    echo "id is : $id"
+    echo "name is : $name"
+    echo "age is : $age"
+    echo "dept is : $department"
+done > /home/ec2-user/linux-shell-scripting/file.csv #(location of file)
+
