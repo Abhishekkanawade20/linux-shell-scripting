09cd7393c6ab58e3e1297d4d21615f55a1ffa88d
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-20 20:21:18 +0530

    commited

--- 24_function_with_argument_02.sh
+++ 24_function_with_argument_02.sh
@@ -3,8 +3,11 @@
 #!/bin/bash
 
 function addition {
-read -p "enter the value : " $a
-read -p "enter the value : " $b    
+#read -p "enter the value : " $a
+#read -p "enter the value : " $b
+
+echo "value of a: $1 "
+echo "value of b: $2 "
 }
 
 addition 24 5
