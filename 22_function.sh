00ea38a5e80201e505841fab45efd848b10a2bbf
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-20 19:58:06 +0530

    commited

---
+++ 22_function.sh
@@ -0,0 +1,17 @@
+# function is used to if we want to repeatative task
+# we can simply define function and whenevr we require we can call that function
+
+#!/bin/bash
+
+function welcome {      # we defined function i.e. welcome
+
+echo "Hi ..........."
+echo " Welcome to team "     # function body
+echo " Thank you !!!!!"
+
+}
+
+welcome                      # we can call function multiple time
+welcome
+welcome
+
