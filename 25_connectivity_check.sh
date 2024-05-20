aa9664dc58e9a402094c7e577c85e4c0d1afa0db
Abhishek <abhishekkanawade2000@gmail.com>
2024-05-20 21:55:44 +0530

    commited

--- 25_connectivity_check.sh
+++ 25_connectivity_check.sh
@@ -1,11 +1,12 @@
+# Here we are going to check connectivity that we are able to ping or not
 
 #!/bin/bash
 
-read -p "Enter site name to check connectivity: " site
-ping -c 1 $site
-sleep 3s
+read -p "Enter site name to check connectivity: " site  #It will take input from user
+ping -c 1 $site                                         #It will ping to site
+                                               #3s interval time
 
-if [[ $? -eq 0 ]]
+if [[ $? -eq 0 ]]                                       #
 then
     echo " able to connect $site"
 else
